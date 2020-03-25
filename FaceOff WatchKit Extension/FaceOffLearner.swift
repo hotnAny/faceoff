c4c728e6cc4bc2f47458e037621451febf5b450c
Xiang 'Anthony' Chen <xac@xiangs-mbp.lan>
2020-03-24 17:59:51 -0700

    updated

---
+++ FaceOff WatchKit Extension/FaceOffLearner.swift
@@ -0,0 +1,9 @@
+//
+//  FaceOffLearner.swift
+//  FaceOff WatchKit Extension
+//
+//  Created by Xiang 'Anthony' Chen on 3/24/20.
+//  Copyright © 2020 Xiang 'Anthony' Chen. All rights reserved.
+//
+
+import Foundation
